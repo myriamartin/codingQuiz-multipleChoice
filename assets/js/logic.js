53f23bc8ff6bee6c04ad1a24b5356f964e2146a7
Myriam <ideasalcuadro@gmail.com>
2023-01-21 00:50:01 +0100

    render questions and add btn styles

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,47 @@
 //VARIABLES
-const startBtn = document.querySelector("#start");
-const startScreen = document.querySelector("#start-screen");
-const questionContainerEl = document.querySelector("#questions");
-const questionTitle = document.querySelector("#question-title");
-const questionChoicesEl = document.querySelector("#choices");
-const feedbackContainerEl = document.querySelector("#feedback");
-const endScreen = document.querySelector("#final-score");
-const finalScore = document.querySelector("#end-screen");
-const quizTimer = document.querySelector("#time");
-const submit = document.querySelector("#submit");
+let startBtn = document.querySelector("#start");
+let startScreen = document.querySelector("#start-screen");
+let questionContainerEl = document.querySelector("#questions");
+let questionTitle = document.querySelector("#question-title");
+let questionChoicesEl = document.querySelector("#choices");
+let feedbackContainerEl = document.querySelector("#feedback");
+let endScreen = document.querySelector("#final-score");
+let finalScore = document.querySelector("#end-screen");
+let quizTimer = document.querySelector("#time");
+let submit = document.querySelector("#submit");
+let initials = document.querySelector("#initials");
+
+//DECLARE TIMER GLOBALLY
+let secondsLeft = 60;
+
+//DECLARE SCORE GLOBALLY
+let score = 0;
+
+//DECLARE CURRENT QUESTION GLOBALLY
+let runningQuestionIndex = 0;
+
+//DECLARE LAST QUESTION GLOBALLY
+let lastQuestionIndex = questions.length - 1;
+
+//START QUIZ
+
+startBtn.addEventListener("click", startQuiz);
+function startQuiz() {
+  startScreen.classList.add("hide");
+  questionContainerEl.classList.remove("hide");
+  renderQuestion();
+}
+
+//RENDER THE QUESTION
+function renderQuestion() {
+  let question = myQuiz[runningQuestionIndex].question;
+  questionTitle.textContent = question;
+  let choices = myQuiz[runningQuestionIndex].choices;
+  questionChoicesEl.textContent = "";
+
+  for (let i = 0; i < choices.length; i++) {
+    let choiceBtn = document.createElement("button");
+    choiceBtn.textContent = choices[i];
+    questionChoicesEl.appendChild(choiceBtn);
+  }
+}
