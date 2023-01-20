090f66c58a3954af85ce6f592df6f75d88456bc0
Myriam <ideasalcuadro@gmail.com>
2023-01-20 23:55:51 +0100

    declare variables and questions array~

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,11 @@
 //VARIABLES
-const startButton = document.querySelector("#start");
-const startContainerEl = document.querySelector("#start-screen");
+const startBtn = document.querySelector("#start");
+const startScreen = document.querySelector("#start-screen");
 const questionContainerEl = document.querySelector("#questions");
 const questionTitle = document.querySelector("#question-title");
-const choices = document.querySelector("#choices");
-const feedback = document.querySelector("#feedback");
-
-// Adds an event listener to the "startButton" element which listens for a "click" event
-// Triggers the "startGame" function when the button is clicked
-startButton.addEventListener("click", startGame);
-
-// Start Quiz
-function startGame() {
-  startContainerEl.classList.add("hide");
-  questionContainerEl.classList.remove("hide");
-}
+const questionChoicesEl = document.querySelector("#choices");
+const feedbackContainerEl = document.querySelector("#feedback");
+const endScreen = document.querySelector("#final-score");
+const finalScore = document.querySelector("#end-screen");
+const quizTimer = document.querySelector("#time");
+const submit = document.querySelector("#submit");
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,91 +1,91 @@
-//array of objectS 
-let questions = [
+//array of objectS
+const myQuiz = [
   {
     question:
       "Which operator returns true if the two compared values are not equal?",
-    answer: "!==",
     choices: ["<>", "~", "==!", "!=="],
+    answer: "!==",
   },
   {
     question: "How is a forEach statement different from a for statement?",
-    answer:
-      "A for statement is generic, but a forEach statement can be used only with an array",
     choices: [
       "Only a for statement uses a callback function.",
       "A for statement is generic, but a forEach statement can be used only with an array.",
       "Only a forEach statement lets you specify your own iterator.",
       "A forEach statement is generic, but a for statement can be used only with an array.",
     ],
+    answer:
+      "A for statement is generic, but a forEach statement can be used only with an array",
   },
   {
     question:
       "Which statement is the correct way to create a variable called rate and assign it the value 100?",
-    answer: "let rate = 100;",
     choices: [
       "let rate = 100;",
       "let 100 = rate;",
       "100 = let rate;",
       "rate = 100;",
     ],
+    answer: "let rate = 100;",
   },
 
   {
     question: "Which expression evaluates to true?",
-    answer: "3 == '3'",
     choices: ["[3] == [3]", "3 == '3'", "3 != '3'", "3 === '3'"],
+    answer: "3 == '3'",
   },
   {
     question:
       "Which Object method returns an iterable that can be used to iterate over the properties of an object?",
-    answer: "Object.keys()",
     choices: [
       "Object.get()",
       "Object.loop()",
       "Object.each()",
       "Object.keys()",
     ],
+    answer: "Object.keys()",
   },
   {
     question: "Which method converts JSON data to a JavaScript object?",
-    answer: "JSON.parse()",
     choices: [
       "JSON.fromString();",
       "JSON.parse()",
       "JSON.toObject()",
       "JSON.stringify()",
     ],
+    answer: "JSON.parse()",
   },
   {
     question: "Which method adds a new element to the beginning of an array?",
-    answer: "unshift()",
     choices: ["shift()", "push()", "unshift()", "pop()"],
+    answer: "unshift()",
   },
 
   {
     question: "Where is the correct place to insert a JavaScript?",
-    answer: "Both <head> or <body> section",
     choices: [
       "The <head> section",
       "The <body> section",
       "The <header> section",
       "Both <head> or <body> section",
     ],
+    answer: "Both <head> or <body> section",
   },
 
   {
     question: "How do you call a function named myFunction",
-    answer: "All is correct",
     choices: [
       "myFunction()",
       "call function myFunction()",
       "call myFunction()",
       "All is correct",
     ],
+    answer: "All is correct",
   },
 
   {
     question: "What API is used to handle objects in localStorage?",
-    answer: "JSON",
     choices: ["DOM", "Web", "Window", "JSON"],
+    answer: "JSON",
   },
 ];
