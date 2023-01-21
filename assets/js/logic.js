db312d8e02c9c8a31aabd42a0f7b73a783bb166a
Myriam <ideasalcuadro@gmail.com>
2023-01-21 02:44:34 +0100

    add question rendered loop

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,10 +12,7 @@ let submit = document.querySelector("#submit");
 let initials = document.querySelector("#initials");
 
 //DECLARE TIMER GLOBALLY
-let secondsLeft = 60;
-
-//DECLARE SCORE GLOBALLY
-let score = 0;
+let time = 90;
 
 //DECLARE CURRENT QUESTION GLOBALLY
 let runningQuestionIndex = 0;
@@ -23,12 +20,22 @@ let runningQuestionIndex = 0;
 //DECLARE LAST QUESTION GLOBALLY
 let lastQuestionIndex = questions.length - 1;
 
+// Will store the textContent of possible answers
+let userChoice = "";
+
 //START QUIZ
 
 startBtn.addEventListener("click", startQuiz);
 function startQuiz() {
-  startScreen.classList.add("hide");
-  questionContainerEl.classList.remove("hide");
+  // Checks if the start section has a class of hide, and if not appends it to it's current class
+  if ((startScreen.getAttribute !== "class", "hide")) {
+    startScreen.setAttribute("class", "hide start");
+  }
+
+  // Checks if the question section has a class of hide, and if not, adds it
+  if ((questionContainerEl.getAttribute !== "class", "hide")) {
+    questionContainerEl.removeAttribute("class");
+  }
   renderQuestion();
 }
 
@@ -40,8 +47,27 @@ function renderQuestion() {
   questionChoicesEl.textContent = "";
 
   for (let i = 0; i < choices.length; i++) {
+    // Assign the current loop of choices to the choice variable
+    let choice = choices[i];
+    // Creates a button and assigns it to a variable;
     let choiceBtn = document.createElement("button");
-    choiceBtn.textContent = choices[i];
+    // Adds the current choice as text to the button
+    choiceBtn.textContent = choice;
+    // Appends the button to the parent questions div
     questionChoicesEl.appendChild(choiceBtn);
+    // Event listener on click on posible answers - event bubbling
+    questionChoicesEl.addEventListener("click", checkAnswer);
   }
+}
+
+//CHECK ANSWER FUNCTION
+function checkAnswer(e) {
+  let selectedAnswer = e.target.innerHTML;
+  console.log(selectedAnswer);
+  let correctAnswer = myQuiz[runningQuestionIndex].correct;
+  console.log(correctAnswer);
+  if (selectedAnswer === correctAnswer) {
+    renderQuestion(runningQuestionIndex++);
+  }
+  
 }
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,22 +1,13 @@
 //array of objectS
+
 const myQuiz = [
   {
     question:
       "Which operator returns true if the two compared values are not equal?",
     choices: ["<>", "~", "==!", "!=="],
-    answer: "!==",
+    correct: "!==",
   },
-  {
-    question: "How is a forEach statement different from a for statement?",
-    choices: [
-      "Only a for statement uses a callback function.",
-      "A for statement is generic, but a forEach statement can be used only with an array.",
-      "Only a forEach statement lets you specify your own iterator.",
-      "A forEach statement is generic, but a for statement can be used only with an array.",
-    ],
-    answer:
-      "A for statement is generic, but a forEach statement can be used only with an array",
-  },
+
   {
     question:
       "Which statement is the correct way to create a variable called rate and assign it the value 100?",
@@ -26,13 +17,13 @@ const myQuiz = [
       "100 = let rate;",
       "rate = 100;",
     ],
-    answer: "let rate = 100;",
+    correct: "let rate = 100;",
   },
 
   {
     question: "Which expression evaluates to true?",
     choices: ["[3] == [3]", "3 == '3'", "3 != '3'", "3 === '3'"],
-    answer: "3 == '3'",
+    correct: "3 == '3'",
   },
   {
     question:
@@ -43,7 +34,7 @@ const myQuiz = [
       "Object.each()",
       "Object.keys()",
     ],
-    answer: "Object.keys()",
+    correct: "Object.keys()",
   },
   {
     question: "Which method converts JSON data to a JavaScript object?",
@@ -53,23 +44,12 @@ const myQuiz = [
       "JSON.toObject()",
       "JSON.stringify()",
     ],
-    answer: "JSON.parse()",
+    correct: "JSON.parse()",
   },
   {
     question: "Which method adds a new element to the beginning of an array?",
     choices: ["shift()", "push()", "unshift()", "pop()"],
-    answer: "unshift()",
-  },
-
-  {
-    question: "Where is the correct place to insert a JavaScript?",
-    choices: [
-      "The <head> section",
-      "The <body> section",
-      "The <header> section",
-      "Both <head> or <body> section",
-    ],
-    answer: "Both <head> or <body> section",
+    correct: "unshift()",
   },
 
   {
@@ -80,12 +60,12 @@ const myQuiz = [
       "call myFunction()",
       "All is correct",
     ],
-    answer: "All is correct",
+    correct: "All is correct",
   },
 
   {
     question: "What API is used to handle objects in localStorage?",
     choices: ["DOM", "Web", "Window", "JSON"],
-    answer: "JSON",
+    correct: "JSON",
   },
 ];
